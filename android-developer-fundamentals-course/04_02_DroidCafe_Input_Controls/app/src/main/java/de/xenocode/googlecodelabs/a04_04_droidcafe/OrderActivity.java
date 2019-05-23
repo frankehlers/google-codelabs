cd7caede97e563be770b59da8337cf125f53b819
Frank Ehlers <ich@frank-ehlers.de>
2019-05-23 23:31:28 +0200

    update codelabs tutorial

--- android-developer-fundamentals-course/04_02_DroidCafe_Input_Controls/app/src/main/java/de/xenocode/googlecodelabs/a04_04_droidcafe/OrderActivity.java
+++ android-developer-fundamentals-course/04_02_DroidCafe_Input_Controls/app/src/main/java/de/xenocode/googlecodelabs/a04_04_droidcafe/OrderActivity.java
@@ -4,21 +4,62 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class OrderActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity implements
+        AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
 
+        // Get the intent and its data.
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.order_textview);
         textView.setText(message);
+
+        // Create the spinner.
+        Spinner spinner = findViewById(R.id.label_spinner);
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+
+        // Create an ArrayAdapter using the string array and default spinner
+        // layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.labels_array,
+                android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner.
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
+    }
+
+    // Interface callback for when any spinner item is selected.
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView,
+                               View view, int i, long l) {
+        String spinnerLabel = adapterView.getItemAtPosition(i).toString();
+        displayToast(spinnerLabel);
+    }
+
+    // Interface callback for when no spinner item is selected.
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        // Do nothing.
     }
 
     public void onRadioButtonClicked(View view) {
@@ -45,7 +86,6 @@ public class OrderActivity extends AppCompatActivity {
             default:
                 // Do nothing.
                 break;
-
         }
     }
 
